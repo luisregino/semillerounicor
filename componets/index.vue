6b03862df722c8f355c7bacc32f33cf590efc175
julianarteaga04 <143424328+julianarteaga04@users.noreply.github.com>
2024-06-22 16:41:47 -0500

    Create index.vue

---
+++ componets/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+import semillero from "../data/dataSemilleros.json";	const props = defineProps({
+    semillero: Object
+});
+let estudiante = ref(semillero[0].facultades[0]);	
+
+</script>
