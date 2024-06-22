0326c430efece7cc3764f4b0e8d1c74f90bb2eef
julianarteaga04 <143424328+julianarteaga04@users.noreply.github.com>
2024-06-22 16:42:15 -0500

    Create Jaafacultades.vue

---
+++ pages/Jaafacultades.vue
@@ -0,0 +1,15 @@
+<script setup>
+const facultades = {
+    nombre: "EduTLan",
+    Facultad: "Educación y Ciencias Humanas",
+    semilleros: [
+            "AVI",
+            "Laboratorio IA",
+    ]
+}
+</script>
+
+<template>
+    <h1>Facultades</h1>
+    <Jaafacultades :facultades="facultades" />
+</template>
