30d4574e97d8236d481ae2614b4dd44b937534ea
luisregino <143423118+luisregino@users.noreply.github.com>
2024-07-08 23:16:48 -0500

    Delete index.vue

--- componets/index.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import semillero from "../data/dataSemilleros.json";	const props = defineProps({
-    semillero: Object
-});
-let estudiante = ref(semillero[0].facultades[0]);	
-
-</script>
