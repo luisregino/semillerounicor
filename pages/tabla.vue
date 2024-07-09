0db60d0eb93c09e000d36ca82bc5ce5b953c8065
luisregino <143423118+luisregino@users.noreply.github.com>
2024-07-08 23:18:00 -0500

    Create tabla.vue

---
+++ pages/tabla.vue
@@ -0,0 +1,17 @@
+<script setup>
+import dataSemilleros from '../data/dataSemilleros'
+</script>
+
+<template>
+    <div class="container px-4">
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Ingeniería" />
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Ciencias de la Salud" />
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Educación y Ciencias Humanas" />
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Ciencias Básicas" />
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Medicina Veterinaria y Zootecnia" />
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Ciencias Agrícolas" />
+      <Micomponente :semillero="dataSemilleros" facultad="Facultad de Ciencias Económicas, Jurídicas y Administrativas" />
+    </div>
+  </template>
+  
+  
